4fad7ff3ee8ad53ed8941d8e6800fbc8ffbeb65f
Raymond_Hasee <notmoirae@foxmail.com>
2020-11-01 17:33:09 +0800

    Add rougnly damage resolve using analyser

---
+++ Assets/Script/Gameplay/Player/Weapon.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prototype.Element;
+
+namespace Prototype.Gameplay.Player
+{
+    public class Weapon : MonoBehaviour
+    {
+        private SpriteRenderer _renderer;
+        [SerializeField] private PixelImageAsset _asset;
+        [SerializeField] private PixelWeaponAnalyser _analyser;
+        // Start is called before the first frame update
+        private void Awake()
+        {
+
+            _analyser = new PixelWeaponAnalyser(_asset.Image, WeaponForwardDirection.TopLeft);
+            _analyser.UpdateWeaponData();
+            _renderer = GetComponent<SpriteRenderer>();
+            
+            //_renderer.
+        }
+
+        public float ResolveDamage(Attack type)
+        {
+            WeaponPixelData[,] data;
+            float damage = 0;
+            switch (type)
+            {
+                case Attack.L:
+                    data = _analyser.WeaponDataLeft;
+                    break;
+                case Attack.M:
+                    data = _analyser.WeaponDataStab;
+                    break;
+                case Attack.R:
+                    data = _analyser.WeaponDataStab;
+                    break;
+                default:
+                    data = null;
+                    break;
+            }
+
+            foreach(WeaponPixelData d in data)
+            {
+                damage += d.Damage;
+            }
+
+            return damage;
+        }
+
+    }
+
+}
+
