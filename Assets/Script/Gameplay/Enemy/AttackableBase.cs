2421ddf2209c13c851fc96a692adab57fe3ffab9
Raymond <notmoirae@foxmail.com>
2021-04-28 23:07:02 +0800

    [feat] Add resource blocks.

--- Assets/Script/Gameplay/Enemy/AttackableBase.cs
+++ Assets/Script/Gameplay/Enemy/AttackableBase.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Prototype.Gameplay.UI;
+using Prototype.Script.Test;
+using Prototype.Element;
+using Prototype.Utils;
+using Prototype.Inventory;
+using Prototype.Settings;
 
 namespace Prototype.Gameplay.Enemy
 {
     public abstract class AttackableBase: MonoBehaviour
     {
+        public bool UseHealthBar;
+        public bool CanDropPixel;
         protected HealthBar healthBar;
+        protected PlayerPackage _package;
         [SerializeField] protected float maxHealth;
         [SerializeField] protected float currentHealth;
+        [SerializeField] protected float _spawnRadius;
+        [SerializeField] protected List<PixelType> _availableTypes = new List<PixelType>();
 
         protected virtual void InitHealthBar()
         {
@@ -19,16 +29,28 @@ namespace Prototype.Gameplay.Enemy
             healthBar.RestoreMaxHealth();
         }
 
-        protected virtual void FindHealthBar()
-        {
+        protected virtual void FindHealthBar() 
+	    { 
             healthBar = transform.Find("UnitCanvas").GetComponentInChildren<HealthBar>();
-        }
+	    }
+
+        protected virtual void InitPackage() 
+	    {
+            _package = GameObject.Find("Player").GetComponent<PlayerPackage>();
+	    }
 
         public virtual void TakeDamage(string type, float damage)
         {
             currentHealth -= damage;
             healthBar.CurrentHealth = currentHealth;
         }
+
+        protected virtual void DropPixel(PixelType type, Vector2 pos)
+	    {
+            var droppedItem = GameObjectPool.Get<DroppedItem>(GamePrefabs.Current.DroppedItem);
+            droppedItem.Init(new Pixel(type));
+            droppedItem.DropAt(pos);
+	    }
     }
 }
 
---
+++ Assets/Script/Gameplay/RoomFacility/PixelCarrier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using Prototype.Inventory;
+using Prototype.Script.Test;
+using Prototype.Utils;
+using Prototype.Element;
+using Prototype.Gameplay.Enemy;
+
+
+public class PixelCarrier :AttackableBase
+{
+
+
+    //public Dictionary<PixelType,>
+
+    private void Start()
+    {
+        _package = GameObject.Find("Player").GetComponent<PlayerPackage>();
+    }
+
+    public override void TakeDamage(string type, float damage) 
+    { 
+		Vector2 offset = Random.insideUnitCircle * _spawnRadius;
+		var pos =  transform.position + MathUtility.ToVector3(offset);
+		_package.Inventory.DropOne(_availableTypes[(int)(Random.value * _availableTypes.Count)], pos);
+    }
+}
